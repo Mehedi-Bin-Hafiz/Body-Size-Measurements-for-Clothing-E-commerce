c59d761cc0f0a24d51c7c1c6a77297f4c0161e86
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2025-04-16 22:34:44 +0600

    django rest api integration

---
+++ api/SizeMeasurementsRestAPI/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for SizeMeasurementsRestAPI project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from sizemeasurements.views import SizeMeasurementsVisionAI, VirtualTry
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('size_measurements/', SizeMeasurementsVisionAI.as_view(), name='size_measure'),
+    path('virtual_try/', VirtualTry.as_view(), name='virtual_try'),
+]
+
