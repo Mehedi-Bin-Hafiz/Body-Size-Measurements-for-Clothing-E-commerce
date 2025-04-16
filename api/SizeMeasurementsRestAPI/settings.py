cf1cfa15d6c08e355a101ccfa48737cbd989a025
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2025-04-16 22:53:30 +0600

    django rest api integration

--- api/SizeMeasurementsRestAPI/settings.py
+++ api/SizeMeasurementsRestAPI/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t4@#71+kmdmjm4+)65^&=%6kwif_r!00*_-%zpfkmt46_#2&s$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
